4dbfaec854b15bfcdbd121ec395a9f8b2a3f8406
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-27 19:22:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elseif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elseif phrase != phrase.upcase
+  else
     return "HUH?! SPEAK UP, SONNY!"
 end
 end
