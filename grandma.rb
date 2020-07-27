80944b5e6a869d9e7f5e270246a244c9d2ed535a
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-27 19:33:05 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elseif phrase == phrase.upcase
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elseif phrase != phrase.upcase
+  else
     return "HUH?! SPEAK UP, SONNY!"
 end
 end
